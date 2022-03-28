bbc6dda53a2b5a00f6af98d4d12b362612d37e21
Khadiza-Aurthee <90924586+Khadiza-Aurthee@users.noreply.github.com>
2022-03-28 12:26:47 -0400

    Draft 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Heading from "./Components/heading";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {};
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Hello React App</h1>
+        <h1>Deaths</h1>
       </div>
     );
   }
+}
+
+function TableRow(props) {
+  let rowStyle = {
+    backgroundColor: "rgb(75, 99, 179)"
+  };
+
+  let fontWeightOponent = {
+    fontWeight: "normal"
+  };
+  let fontWeightTeam = {
+    fontWeight: "normal"
+  };
 }
 
 export default App;
---
+++ src/Components/heading.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./heading.css";
+
+function Heading() {
+  return (
+    <div className="Heading">
+      <h1>BOMBACLAAT</h1>
+    </div>
+  );
+}
+
+export default Heading;
